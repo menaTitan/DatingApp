6285b9c8ba950e298ba08a698c5e7c9f658826c5
Mena <msergeyous@gmail.com>
2021-03-08 08:47:15 -0600

    added angular module

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating app';
+  users : any;
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(reponse => {
+      this.users = reponse;
+
+    }, error=>{
+      console.log(error);
+    })
+  }
+}
