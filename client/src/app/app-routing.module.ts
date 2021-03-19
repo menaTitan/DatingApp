b24d54f3b85998e034a4f071df084937d905b837
Mena Sergeyous <Menas@localgovcorp.com>
2021-03-19 16:04:10 -0500

    added edit profile page for a user

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -19,8 +20,9 @@ const routes: Routes = [
     children:[
       {path:'members', component: MemberListComponent},
       {path:'members/:username', component: MemberDetailsComponent},
+      {path:'member/edit', component: MemberEditComponent},
       {path:'lists', component: ListsComponent},
-      {path:'messages', component: MessagesComponent}
+      {path:'messages', component: MessagesComponent},
     ]
   },
   {path:'server-error', component: ServerErrorComponent},
