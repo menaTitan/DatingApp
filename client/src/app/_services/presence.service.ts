30eea9f87744abbd3c4db5786a64bbb943e81b91
Mena Sergeyous <Menas@localgovcorp.com>
2021-05-19 10:29:31 -0500

    deployed fix for inbox messages

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -4,7 +4,7 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
